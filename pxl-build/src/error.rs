539f10138db5ac8e2fdfde8b894876253574c259
Casey Rodarmor <casey@rodarmor.com>
2018-06-25 16:08:47 -0400

    Reorganize into workspace and add pxl-build [WIP]

---
+++ pxl-build/src/error.rs
@@ -0,0 +1,55 @@
+use std::{
+    io, path::{Path, PathBuf},
+};
+
+use image;
+
+#[derive(Debug)]
+pub enum Error {
+    Io {
+        io_error: io::Error,
+        path: PathBuf,
+    },
+    Image {
+        image_error: image::ImageError,
+        path: PathBuf,
+    },
+    IsNotDirectory {
+        path: PathBuf,
+    },
+    NonUnicodePath {
+        path: PathBuf,
+    },
+    ConflictingIdentifiers {
+        a: PathBuf,
+        b: PathBuf,
+        identifier: String,
+    },
+    FilenameNotValidRustIdentifier {
+        path: PathBuf,
+    },
+    MissingExtension {
+        path: PathBuf,
+    },
+    UnsupportedExtension {
+        path: PathBuf,
+    },
+}
+
+impl<'path> From<(io::Error, &'path Path)> for Error {
+    fn from(tuple: (io::Error, &'path Path)) -> Error {
+        Error::Io {
+            io_error: tuple.0,
+            path: tuple.1.to_path_buf(),
+        }
+    }
+}
+
+impl<'path> From<(image::ImageError, &'path Path)> for Error {
+    fn from(tuple: (image::ImageError, &'path Path)) -> Error {
+        Error::Image {
+            image_error: tuple.0,
+            path: tuple.1.to_path_buf(),
+        }
+    }
+}
